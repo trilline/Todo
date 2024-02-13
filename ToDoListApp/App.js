3bf7f83a23a62bf65e123c4b450e03a1fcb7bb0c
maeva-merveille.ngoban <maeva-merveille.ngoban@afnor.org>
2024-02-13 11:18:09 +0100

    essai mise en place du background

--- ToDoListApp/App.js
+++ ToDoListApp/App.js
@@ -1,21 +1,32 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
-import TodoList from './TodoList'; //le todo comp
+import { StyleSheet, View, ImageBackground } from 'react-native';
+import TodoList from './TodoList'; 
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <TodoList /> 
-      <StatusBar style="auto" />
-    </View>
+    <ImageBackground
+      source={require('./assets/th.jpg')} 
+      style={styles.background}
+    >
+      <View style={styles.container}>
+        <TodoList /> 
+        <StatusBar style="auto" />
+      </View>
+    </ImageBackground>
   );
 }
 
 const styles = StyleSheet.create({
+  background: {
+    flex: 1,
+    resizeMode: 'cover', 
+    justifyContent: 'center', 
+  },
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    paddingTop: 40, 
+    backgroundColor: 'rgba(0,0,0,0.5)', 
+    paddingVertical: 40, 
+    paddingHorizontal: 20,
   },
 });
